abaeeb93d7be752e44ba66d6e5ea41c6a6c54cff
mr-mikolajczyk <159031258+mr-mikolajczyk@users.noreply.github.com>
2024-02-05 17:22:28 +0100

    Update services.js

--- services.js
+++ services.js
@@ -9,7 +9,7 @@ gsap.timeline({
     },
   });
 
-  gsap.set(".aside_item",{opacity:0, yPercent: 20})
+  gsap.set(".aside_item",{opacity:0, y: 50})
   ScrollTrigger.batch(".aside_item", {
     start: "top 70%",
     onEnter: batch => gsap.to(batch, {yPercent: 0, opacity: 1, ease:"power3.out", stagger: 0.1, duration: 1}),
