4c5075052175da7f251f763b84289346c5f82523
mr-mikolajczyk <159031258+mr-mikolajczyk@users.noreply.github.com>
2024-02-05 17:19:52 +0100

    Update services.js

--- services.js
+++ services.js
@@ -8,6 +8,13 @@ gsap.timeline({
       end: "bottom 80",
     },
   });
+
+  gsap.set(".aside_item",{opacity:0, yPercent: 20})
+  ScrollTrigger.batch(".aside_item", {
+    start: "top 70%",
+    onEnter: batch => gsap.to(batch, {yPercent: 0, opacity: 1, ease:"power3.out", stagger: 0.1, duration: 1}),
+  });
+
   gsap.set(".services_item",{opacity:0, yPercent: 20})
   ScrollTrigger.batch(".services_item", {
     start: "top 70%",
