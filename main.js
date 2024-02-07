430bc7d82c1b0e8e0de7a48e8de781fad1daae00
mr-mikolajczyk <159031258+mr-mikolajczyk@users.noreply.github.com>
2024-02-07 10:08:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -88,6 +88,22 @@
     });
   });
   
+  $(function() {  
+    $('.button')
+      .on('mouseenter', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      });
+  });
+
   //date
   let today = new Date();
   let year = today.getFullYear();
