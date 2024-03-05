c379c0f4d4e39266f5f21d65e9e0642da2a8eebd
mr-mikolajczyk <159031258+mr-mikolajczyk@users.noreply.github.com>
2024-03-05 08:57:41 +0100

    8:57

---
+++ projects.js
@@ -0,0 +1,29 @@
+const portfolioItems = document.querySelectorAll('.home_portfolio_item');
+
+portfolioItems.forEach(item => {
+  const videoElement = item.querySelector('video'); // Target video element within item
+
+  if (videoElement) { // Ensure video element exists
+    item.addEventListener('mouseover', () => {
+      portfolioItems.forEach(otherItem => {
+        if (otherItem !== item) {
+          otherItem.classList.add('inactive');
+        }
+      });
+
+      videoElement.play(); // Play video
+    });
+
+    item.addEventListener('mouseout', () => {
+      portfolioItems.forEach(otherItem => {
+        if (otherItem !== item) {
+          otherItem.classList.remove('inactive');
+        }
+      });
+
+      videoElement.pause(); // Pause video
+    });
+  } else {
+    console.warn(`Video element not found in item: ${item}`);
+  }
+});
--- work.js
+++ work.js
@@ -1,20 +1,20 @@
 
   //// ----IMG-Reveal----
-  let imgFade = gsap.utils.toArray("img");
-  imgFade.forEach((item, index) => {
-    let tl = gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: item,
-          start: "top 90%",
-        },
-      })
-      .from(item, {
-        clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
-        duration: 1.5,
-        ease: "power2.out",
-      });
-  });
+  // let imgFade = gsap.utils.toArray("img");
+  // imgFade.forEach((item, index) => {
+  //   let tl = gsap
+  //     .timeline({
+  //       scrollTrigger: {
+  //         trigger: item,
+  //         start: "top 90%",
+  //       },
+  //     })
+  //     .from(item, {
+  //       clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
+  //       duration: 1.5,
+  //       ease: "power2.out",
+  //     });
+  // });
   
   gsap.utils.toArray(".portfolio_works_image").forEach((el, i) => {
     let portfolioImg = gsap.timeline({
@@ -46,3 +46,34 @@
       duration: 0.8,
       ease: "power2.inOut",
     });
+
+
+    const portfolioItems = document.querySelectorAll('.more_portfolio-item');
+
+portfolioItems.forEach(item => {
+  const videoElement = item.querySelector('video'); // Target video element within item
+
+  if (videoElement) { // Ensure video element exists
+    item.addEventListener('mouseover', () => {
+      portfolioItems.forEach(otherItem => {
+        if (otherItem !== item) {
+          otherItem.classList.add('inactive');
+        }
+      });
+
+      videoElement.play(); // Play video
+    });
+
+    item.addEventListener('mouseout', () => {
+      portfolioItems.forEach(otherItem => {
+        if (otherItem !== item) {
+          otherItem.classList.remove('inactive');
+        }
+      });
+
+      videoElement.pause(); // Pause video
+    });
+  } else {
+    console.warn(`Video element not found in item: ${item}`);
+  }
+});
